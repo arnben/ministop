b4c6fceeb4d1e6523f8989991537bc0fc0b8c90c
Alben Magsombol <amagsomb@MGC12W426HHV2R.fbpld77.ford.com>
2020-06-22 14:18:19 -0400

    Inital commit

---
+++ src/main/java/com/alben/ministop/MinistopApplication.java
@@ -0,0 +1,13 @@
+package com.alben.ministop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinistopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinistopApplication.class, args);
+	}
+
+}
