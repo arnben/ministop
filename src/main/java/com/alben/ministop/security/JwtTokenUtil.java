9ebbeea9388a8222fc9102e854c7c74f487726b4
Alben Magsombol <amagsomb@MGC12W426HHV2R.fbpld77.ford.com>
2020-06-28 15:34:59 -0400

    WIP start implementing security

---
+++ src/main/java/com/alben/ministop/security/JwtTokenUtil.java
@@ -0,0 +1,12 @@
+package com.alben.ministop.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+}
