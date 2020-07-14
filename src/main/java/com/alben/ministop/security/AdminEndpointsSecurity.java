284762567b79fc66bd84a1c99a3e855b3b6c1705
Alben Magsombol <amagsomb@MGC12W426HHV2R.fbpld77.ford.com>
2020-07-14 16:45:18 -0400

    Implement interceptor for admin endpoints

---
+++ src/main/java/com/alben/ministop/security/AdminEndpointsSecurity.java
@@ -0,0 +1,48 @@
+package com.alben.ministop.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+public class AdminEndpointsSecurity implements WebMvcConfigurer {
+
+    @Value("${ministop.admin.rootPath}")
+    private String adminContextPath;
+
+    @Autowired
+    private AdminEndpointInterceptor adminEndpointInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminEndpointInterceptor).addPathPatterns(adminContextPath + "/**");
+    }
+
+    @Slf4j
+    @Component
+    public static class AdminEndpointInterceptor implements HandlerInterceptor {
+
+        @Value("${ministop.admin.secret}")
+        private String adminKey;
+
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            log.info("Calling admin endpoint.");
+            String inputKey = request.getHeader("adminKey");
+            if(adminKey.equals(inputKey)) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+    }
+}
