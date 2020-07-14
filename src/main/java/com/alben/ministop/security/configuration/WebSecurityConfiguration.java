e1e5d3879acc769004449298c690c353005816e3
Alben Magsombol <amagsomb@MGC12W426HHV2R.fbpld77.ford.com>
2020-07-14 16:09:37 -0400

    Disabled oauth security temp

--- src/main/java/com/alben/ministop/security/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/alben/ministop/security/configuration/WebSecurityConfiguration.java
@@ -2,18 +2,11 @@ package com.alben.ministop.security.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 //Reference: https://www.toptal.com/spring/spring-boot-oauth2-jwt-rest-protection
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration {//extends WebSecurityConfigurerAdapter {
 
     @Value("${ministop.api.rootPath}")
     private String apiContextPath;
@@ -25,16 +18,16 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     private MinistopAuthenticationEntryPoint ministopAuthenticationEntryPoint;
 
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-            .authorizeRequests()
-            .antMatchers(apiContextPath + "/**").hasRole("USER")
-            .antMatchers(adminContextPath + "/**").hasRole("ADMIN")
-            .anyRequest().permitAll()
-        .and()
-            .exceptionHandling().authenticationEntryPoint(ministopAuthenticationEntryPoint);
-    }
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http
+//            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//        .and()
+//            .authorizeRequests()
+//            .antMatchers(apiContextPath + "/**").hasRole("USER")
+//            .antMatchers(adminContextPath + "/**").hasRole("ADMIN")
+//            .anyRequest().permitAll()
+//        .and()
+//            .exceptionHandling().authenticationEntryPoint(ministopAuthenticationEntryPoint);
+//    }
 }
